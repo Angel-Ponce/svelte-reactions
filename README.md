2c6b6fd983bfdfe2a152a29889bee31f4807474d
Angel Ponce <acdpf000@gmail.com>
2022-08-12 16:09:01 -0600

    initial styles for reaction component 🎨

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-Testing
-
 # create-svelte
 
 Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
--- src/lib/Reaction.svelte
+++ src/lib/Reaction.svelte
@@ -1,3 +1,46 @@
-<script lang="ts"></script>
+<script lang="ts">
+	export let reaction = '🚀';
+	export let quantity = 0;
+</script>
 
-Reaction component
+<div class="reaction">
+	<span class="emoji">
+		{reaction}
+		{#if quantity > 0}
+			{quantity}
+		{/if}
+	</span>
+</div>
+
+<style>
+	.reaction {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 2px;
+		width: fit-content;
+		height: auto;
+		border-radius: 4px;
+		background-color: #ffffff;
+		cursor: pointer;
+		transition-duration: 500ms;
+		user-select: none;
+		padding: 3px;
+	}
+
+	.reaction:hover {
+		background-color: #f0f0f0;
+	}
+
+	.reaction .emoji {
+		font-size: 1rem;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
+			Geneva, Verdana, sans-serif;
+		color: #888888;
+	}
+
+	.reaction .emoji:active {
+		transform: scale(0.85);
+		transition-duration: inherit;
+	}
+</style>
